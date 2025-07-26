e75b47fcafcf1c2fbcc1a42eebbee3f819b8d4e9
MarwaKHTanani <tananimarwa@gmail.com>
2025-07-26 15:42:32 +0300

    format

--- stage-1/exercise/app.js
+++ stage-1/exercise/app.js
@@ -11,117 +11,116 @@
  * Take your time, and think about what principles you are trying to apply while
  * you are refactoring.
  */
-'use strict';
-const formUnanswerd=document.querySelector('#form-unanswered');
-const resultSummary=document.querySelector('#results-summary')
-const resultBody=document.querySelector('#results-body');
+"use strict";
+const formUnanswerd = document.querySelector("#form-unanswered");
+const resultSummary = document.querySelector("#results-summary");
+const resultBody = document.querySelector("#results-body");
 
-
-
-const req =(url, callback)=>{
-  let xhr= new XMLHttpRequest();
-  xhr.addEventListener('load',()=>{
+const req = (url, callback) => {
+  let xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", () => {
     if (xhr.status === 200) {
       let response = JSON.parse(xhr.responseText);
-      callback(response)
-      } else {
-      console.log('Status Code: ' + xhr.status);
+      callback(response);
+    } else {
+      console.log("Status Code: " + xhr.status);
     }
-})
-  xhr.open('GET', url);
+  });
+  xhr.open("GET", url);
   xhr.send();
-}
+};
 
-function createElement(tag ) {
+function createElement(tag) {
   const el = document.createElement(tag);
   return el;
 }
 
-formUnanswerd.addEventListener('submit',(e)=>{
-  e.preventDefault()
-  let form =e.target;
-  let tags=form.querySelector('input[name=tags]').value;
-  let url  = 'https://api.stackexchange.com/2.2/questions/unanswered?order=desc&sort=activity&site=stackoverflow&tagged=' + tags;
+formUnanswerd.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let form = e.target;
+  let tags = form.querySelector("input[name=tags]").value;
+  let url =
+    "https://api.stackexchange.com/2.2/questions/unanswered?order=desc&sort=activity&site=stackoverflow&tagged=" +
+    tags;
 
- req(url,(response)=>{
+  req(url, (response) => {
+    resultSummary.innerHTML = "";
 
-      resultSummary.innerHTML=''
+    const p = createElement("p");
+    p.textContent = `Query of ${tags} returned ${response.items.length} results`;
+    resultSummary.appendChild(p);
 
-      const p = createElement('p')
-      p.textContent=`Query of ${tags} returned ${response.items.length} results`;
-      resultSummary.appendChild(p)
+    resultBody.innerHTML = "";
+    response.items.map(function (item) {
+      const container = createElement("div");
 
-      resultBody.innerHTML='';
-      response.items.map(function (item) {
-        const container=createElement('div')
+      const title = createElement("p");
+      title.textContent = `Title: ${item.title}`;
+      container.appendChild(title);
 
-        const title=createElement('p')
-        title.textContent=`Title: ${item.title}`
-        container.appendChild(title)
+      const date = createElement("p");
+      date.textContent = `Date: ${new Date(item.creation_date)}`;
+      container.appendChild(date);
 
-        const date=createElement('p')
-        date.textContent=`Date: ${new Date(item.creation_date)}`
-        container.appendChild(date)
+      const link = createElement("p");
+      link.textContent = "Link: ";
+      const a = createElement("a");
+      a.href = item.link;
+      a.textContent = "Click here";
+      link.appendChild(a);
+      container.appendChild(link);
 
-        const link=createElement('p')
-        link.textContent='Link: '
-        const a=createElement('a')
-        a.href=item.link
-        a.textContent='Click here'
-        link.appendChild(a)
-        container.appendChild(link)
+      const owner = createElement("p");
+      owner.textContent = `Owner: ${item.owner.display_name}`;
+      container.appendChild(owner);
 
-        const owner=createElement('p')
-        owner.textContent=`Owner: ${ item.owner.display_name}`
-        container.appendChild(owner)
+      resultBody.appendChild(container);
+    });
+  });
+});
 
-        resultBody.appendChild(container)
+const formAnswerd = document.querySelector("#form-answerers");
+formAnswerd.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let form = e.target;
+  let tags = form.querySelector("input[name=tags]").value;
+  let url =
+    "http://api.stackexchange.com/2.2/tags/" +
+    tags +
+    "/top-answerers/all_time?site=stackoverflow";
 
-      })
-    })
-  })
+  req(url, (response) => {
+    resultSummary.innerHTML = "";
 
+    const p = createElement("p");
+    p.textContent = `Query of ${tags} returned ${response.items.length} results`;
+    resultSummary.appendChild(p);
 
-const formAnswerd=document.querySelector('#form-answerers');
-formAnswerd.addEventListener('submit',(e)=>{
-  e.preventDefault()
-  let form =e.target;
-  let tags=form.querySelector('input[name=tags]').value;
-  let url  = 'http://api.stackexchange.com/2.2/tags/' + tags + '/top-answerers/all_time?site=stackoverflow'
+    resultBody.innerHTML = "";
+    response.items.map(function (item) {
+      const container = createElement("div");
 
- req(url,(response)=>{
-      resultSummary.innerHTML=''
+      const user = createElement("p");
+      user.textContent = `User: ${item.user.display_name}`;
+      container.appendChild(user);
 
-      const p = createElement('p')
-      p.textContent=`Query of ${tags} returned ${response.items.length} results`;
-      resultSummary.appendChild(p)
+      const reputation = createElement("p");
+      reputation.textContent = `Reputation: ${item.user.reputation}`;
+      container.appendChild(reputation);
 
-      resultBody.innerHTML='';
-      response.items.map(function (item) {
-        const container=createElement('div')
+      const profile = createElement("p");
+      profile.textContent = "Profile: ";
+      const a = createElement("a");
+      a.href = item.user.link;
+      a.textContent = "Click here";
+      profile.appendChild(a);
+      container.appendChild(profile);
 
-        const user=createElement('p')
-        user.textContent=`User: ${item.user.display_name}`
-        container.appendChild(user)
+      const score = createElement("p");
+      score.textContent = `Score: ${item.score}`;
+      container.appendChild(score);
 
-        const reputation=createElement('p')
-        reputation.textContent=`Reputation: ${item.user.reputation}`
-        container.appendChild(reputation)
-
-        const profile=createElement('p')
-        profile.textContent='Profile: '
-        const a=createElement('a')
-        a.href=item.user.link
-        a.textContent='Click here'
-        profile.appendChild(a)
-        container.appendChild(profile)
-
-        const score=createElement('p')
-        score.textContent=`Score: ${item.score}`
-        container.appendChild(score)
-
-        resultBody.appendChild(container)
-
-      })
-})
-})
+      resultBody.appendChild(container);
+    });
+  });
+});
